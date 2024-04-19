08c8beb42060815df8378eaea7eb02485208bd51
tobyrushton <tobyjrushton@icloud.com>
2024-04-19 21:54:56 +0100

    added scraping for trainline return

--- packages/functions/src/scrapes/railyeasy_test.go
+++ packages/functions/src/scrapes/railyeasy_test.go
@@ -1,22 +1,22 @@
 package scrapes
 
-import (
-	"fmt"
-	"testing"
-)
+// import (
+// 	"fmt"
+// 	"testing"
+// )
 
-func TestRaileasy(t *testing.T) {
-	req := Request{
-		Origin:      "SAC",
-		Destination: "STP",
-		Departure:   "2024-05-19T17:26:25Z",
-		Return:      "2024-05-20T12:26:25Z",
-	}
+// func TestRaileasy(t *testing.T) {
+// 	req := Request{
+// 		Origin:      "SAC",
+// 		Destination: "STP",
+// 		Departure:   "2024-05-19T17:26:25Z",
+// 		Return:      "2024-05-20T12:26:25Z",
+// 	}
 
-	res, err := ScrapeRaileasy(req)
-	if err != nil {
-		t.Error(err)
-	}
+// 	res, err := ScrapeRaileasy(req)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-	fmt.Println(res)
-}
+// 	fmt.Println(res)
+// }
--- packages/functions/src/scrapes/trainline.go
+++ packages/functions/src/scrapes/trainline.go
@@ -42,11 +42,6 @@ func ScrapeTrainline(req Request) (ScrapeResults, error) {
 	outHour := out.Hour()
 	outMin := out.Minute()
 
-	// in, err := time.Parse(iso8601Layout, req.Return)
-	// if err != nil {
-	// 	return nil, errors.New("Invalid date")
-	// }
-
 	outStation, err := getStationByCode(req.Origin)
 	if err != nil {
 		return nil, err
@@ -75,9 +70,29 @@ func ScrapeTrainline(req Request) (ScrapeResults, error) {
 		"ExtendedSearch":            "Get times & tickets",
 	}
 
+	if req.Return != "" {
+		in, err := time.Parse(iso8601Layout, req.Return)
+		if err != nil {
+			return nil, errors.New("invalid date")
+		}
+		inDay := in.Day()
+		inMonth := in.Month()
+		inYear := in.Year()
+		inHour := in.Hour()
+		inMin := in.Minute()
+
+		form["returnDate"] = fmt.Sprintf("%d-%d-%d", inYear, inMonth, inDay)
+		form["ReturnHour"] = fmt.Sprintf("%d", inHour)
+		form["ReturnMinute"] = fmt.Sprintf("%d", inMin)
+	}
+
 	c := colly.NewCollector()
 
 	var res ScrapeResults
+
+	// c.OnHTML("html", func(e *colly.HTMLElement) {
+	// 	e.Response.Save("trainline.html")
+	// })
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		data := e.ChildAttr("form", "data-defaults")
@@ -85,25 +100,44 @@ func ScrapeTrainline(req Request) (ScrapeResults, error) {
 		var results scrapedJson
 		json.Unmarshal([]byte(data), &results)
 
-		for _, ticket := range results.FullJourneys[0].StandardTickets[0].Tickets {
-			result := ScrapeResult{}
-			if priceVal, ok := ticket["price"].(string); ok {
-				result.Price = utils.PriceToFloat(priceVal)
-			}
-			if departureVal, ok := ticket["departureDateTime"].(string); ok {
-				result.DepartureTime = departureVal
-			}
-			if arrivalVal, ok := ticket["arrivalDateTime"].(string); ok {
-				result.ArrivalTime = arrivalVal
-			}
-			// TODO: Error handling if one is wrong
-			// TODO: Add link
-			// fmt.Println(ticket)
-			res = append(res, result)
+		res = getTrainlinePrices(results, true)
+
+		if req.Return != "" {
+			res = append(res, getTrainlinePrices(results, false)...)
 		}
 	})
 
 	c.Post(trainlineUrl, form)
 
 	return res, nil
+}
+
+func getTrainlinePrices(data scrapedJson, outbound bool) ScrapeResults {
+	var index int
+	if outbound {
+		index = 0
+	} else {
+		index = 1
+	}
+
+	var res ScrapeResults
+
+	for _, ticket := range data.FullJourneys[index].StandardTickets[0].Tickets {
+		result := ScrapeResult{}
+		if priceVal, ok := ticket["price"].(string); ok {
+			result.Price = utils.PriceToFloat(priceVal)
+		}
+		if departureVal, ok := ticket["departureDateTime"].(string); ok {
+			result.DepartureTime = departureVal
+		}
+		if arrivalVal, ok := ticket["arrivalDateTime"].(string); ok {
+			result.ArrivalTime = arrivalVal
+		}
+		// TODO: Error handling if one is wrong
+		// TODO: Add link
+		// fmt.Println(ticket)
+		res = append(res, result)
+	}
+
+	return res
 }
