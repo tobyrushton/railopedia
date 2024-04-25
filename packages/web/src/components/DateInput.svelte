8c2d2695039d170c8c91370453ca7829c5e7e07b
tobyrushton <tobyjrushton@icloud.com>
2024-04-25 23:24:47 +0100

    added date input component

---
+++ packages/web/src/components/DateInput.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { DatePicker } from 'date-picker-svelte'
+    import { clickoutside } from '@svelte-put/clickoutside'
+    import { Calendar } from 'lucide-svelte'
+    import dayjs from 'dayjs'
+
+    let date = new Date()
+
+    let inputSelected = false
+
+    let formatedDate = ""
+
+    $: formatedDate = dayjs(date).format('HH:mm ddd D MMM')
+</script>
+
+<div
+    class="flex flex-col w-80"
+    use:clickoutside
+    on:clickoutside={() => inputSelected = false}
+>
+    <label class="flex relative bg-ternary rounded focus:outline-none p-2 gap-2">
+        <Calendar class="pointer-events-none size-6" />
+        <input 
+            class="focus:outline-none w-full bg-inherit"
+            type="text"
+            on:click={() => inputSelected = !inputSelected}
+            value={formatedDate}
+            readonly 
+        />
+    </label>
+    {#if inputSelected}
+        <DatePicker bind:value={date} min={new Date()} timePrecision="minute" class="w-80"/>
+    {/if}
+</div>
