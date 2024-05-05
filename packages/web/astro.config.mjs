10091362f3206e40e73fc628ef67ec3d5e7b826c
tobyrushton <tobyjrushton@icloud.com>
2024-05-05 14:54:26 +0100

    added loading fallback for search

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
-import aws from 'astro-sst';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
+import aws from 'astro-sst'
 
-import simpleStackStream from "simple-stack-stream";
+import simpleStackStream from 'simple-stack-stream'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), simpleStackStream()],
-  output: 'hybrid',
-  adapter: aws()
-});
+    integrations: [tailwind(), svelte(), simpleStackStream()],
+    output: 'hybrid',
+    adapter: aws(),
+})
