b5b68db38efab20ddf7e14806f760246f272f321
tobyrushton <tobyjrushton@icloud.com>
2024-05-06 02:20:40 +0100

    fix tests

--- packages/functions/src/scrapes/railyeasy_test.go
+++ packages/functions/src/scrapes/railyeasy_test.go
@@ -27,7 +27,7 @@ var TestRequestReturn = Request{
 }
 
 var timeRegexString = "(?i)[0-9]+:[0-9]+"
-var isoRegexString = "(?i)[0-9]+-[0-9]+-[0-9]+T[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]{1,3})?Z"
+var isoRegexString = "(?i)[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+"
 
 var timeRegex = regexp.MustCompile(timeRegexString)
 var isoRegex = regexp.MustCompile(isoRegexString)
