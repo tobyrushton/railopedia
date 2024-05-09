f635a2d4862def8f5eb59ece232085e13eb571eb
tobyrushton <tobyjrushton@icloud.com>
2024-05-09 17:48:27 +0100

    added RailcardInput

---
+++ packages/web/src/components/RailcardInput.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { railcards, type Railcard } from '../utils/railcard'
+    export let value: Railcard
+
+</script>
+
+<select 
+    class="w-80 rounded bg-ternary p-2"
+    bind:value={value}
+>
+    {#each Object.entries(railcards) as [code, name]}
+        <option value={code} selected={code === "N"}>
+            {name}
+        </option>
+    {/each}
+</select>
